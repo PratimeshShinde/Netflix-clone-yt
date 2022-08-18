9b5bb6274121d621db06dd2ce17be214779a8782
Prati <pratimeshshinde121@gmail.com>
2022-08-18 19:30:27 +0530

    My List.Build Done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import { getProducts, Product } from "@stripe/firestore-stripe-payments";
 import Head from "next/head";
 import { useRecoilValue } from "recoil";
-import { modalState } from "../atoms/ModalAtom";
+import { modalState, movieState } from "../atoms/ModalAtom";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 import Modal from "../components/Modal";
 import Plans from "../components/Plans";
 import Row from "../components/Row";
 import useAuth from "../hooks/useAuth";
+import useList from "../hooks/useList";
 import useSubscription from "../hooks/useSubscription";
 import payments from "../lib/stripe";
 import { Movie } from "../typings";
@@ -40,6 +41,9 @@ const Home = ({
   const { loading,user } = useAuth();
   const showModal = useRecoilValue(modalState);
   const subscription = useSubscription(user);
+  const movie = useRecoilValue(movieState);
+  const list = useList(user?.uid)
+
 
   if (loading || subscription === null) return null;
 
@@ -64,7 +68,7 @@ const Home = ({
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
           {/* My List Components*/}
-          
+          {list.length > 0 && <Row title="My List" movies={list} />}
 
           <Row title="Comedies" movies={comedyMovies} />
           <Row title="Scary Movies" movies={horrorMovies} />
